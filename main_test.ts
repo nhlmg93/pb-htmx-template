48accf41694d4936ffdbcda0aeaa94fdb2a7b627
Nathan Helmig <nhelmig93@gmail.com>
2026-01-11 09:42:28 -0600

    batman

---
+++ main_test.ts
@@ -0,0 +1,25 @@
+import puppeteer, { type Browser, type Page } from "puppeteer-core";
+
+const BASE_URL = "http://127.0.0.1:8090";
+
+let browser: Browser;
+let page: Page;
+
+Deno.test.beforeAll(async () => {
+  browser = await puppeteer.launch({ executablePath: "/usr/bin/chromium" });
+  page = await browser.newPage();
+  page.on("console", (msg) => console.debug(`PAGE LOG: ${msg.text()}`));
+  await page.goto(BASE_URL);
+});
+
+Deno.test.afterAll(async () => {
+  await browser.close();
+});
+
+Deno.test({
+  name: "GET /",
+  sanitizeResources: false,
+  sanitizeOps: false,
+  async fn() {
+  },
+});
---
+++ pb_hooks/main.pb.js
@@ -0,0 +1,26 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+routerAdd("get", "/health", (e) => {
+  return e.string(200, "OK");
+});
+
+//const log = (msg) =>
+//  console.log(
+//    `\x1b[90m[DEBUG]\x1b[0m \x1b[36m${msg}\x1b[0m`,
+//  );
+routerAdd("get", "/", (e) => {
+  const html = $template.loadFiles(
+    `${__hooks}/views/layout.html`,
+    `${__hooks}/views/home.html`,
+  ).render({});
+
+  return e.html(200, html);
+});
+routerAdd("get", "/workout", (e) => {
+  const html = $template.loadFiles(
+    `${__hooks}/views/layout.html`,
+    `${__hooks}/views/workout.html`,
+  ).render({});
+
+  return e.html(200, html);
+});
